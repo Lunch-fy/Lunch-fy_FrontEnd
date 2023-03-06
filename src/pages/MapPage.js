c46615c8356e66612f17c94c1d5b051dd88aaca3
s1uth <handtaein@gmail.com>
2023-03-07 03:49:20 +0900

    commit

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect,useState } from 'react';
 import KakaoMapApi from '../components/api/KakaoMapApi';
 import logo from '../assets/logo2.svg';
-import style from '../css/MapPage.css';
+import style from '../css/Mappage.css';
 import SearchResultCard from '../components/card/SearchResultCard';
 import StoreInfoCard from '../components/card/StoreInfoCard';
 
