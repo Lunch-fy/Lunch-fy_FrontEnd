9e98d846704fa07995835d6c2e678245a522a5c7
LSY <112356856+OWL-AT-Midnight@users.noreply.github.com>
2023-03-06 12:05:48 +0900

    feat : 중간 커밋

--- src/components/start/InfoModal.js
+++ src/components/start/InfoModal.js
@@ -1,15 +1,17 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import style from './InfoModal.css';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import { Checkbox, FormControlLabel, TextField, Radio, FormControl, FormLabel, RadioGroup } from '@mui/material';
 
 const styled = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 1200,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
@@ -17,27 +19,36 @@ const styled = {
   };
 
 const InfoModal = () => {
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(false);
+    const [location, setLocation] = useState();
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
     return (
     <>
       <Button onClick={handleOpen}>점심 찾으러 가기!</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
+
+      <Modal open={open} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+
         <Box sx={styled}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
+          <Button variant='text'style={{float: 'right'}} onClick={handleClose}>창 닫기</Button>
+          <Typography id="modal-modal-title" variant="h6" component="h2">점심 찾기</Typography>
+
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
+            <FormControl>
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="row-radio-buttons-group"
+              >
+                <FormControlLabel value="roadLocation" control={<Radio/>} label="도로명주소" />
+                <FormControlLabel value="buildingLocation" control={<Radio/>} label="건물이름" />
+              </RadioGroup>
+            </FormControl>
+            <TextField fullWidth label='도로명 주소' variant='outlined' className='textInput'></TextField>
+            <TextField fullWidth label='건물 이름' variant='outlined'></TextField>
+            </Typography>
+          </Box>
       </Modal>
     </>
     );
